5c10a252f9d75af36d6fd264f56c0c6f1baa8177
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-15 14:55:55 +0600

    Vue Js Reactivity Fundamentals

---
+++ reactivity-fundamentals/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed, ref } from 'vue'
+let msg = ref("Vue Js Reactivity Fundamentals")
+
+
+setTimeout(() => {
+  msg.value = "Vue Js Reactivity Hello World"
+}, 5000);
+
+const firsName = ref("Devil");
+const lastName = ref("Doe");
+const fullName = () => {
+  return firsName.value + ' ' + lastName.value;
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>Hello: {{ fullName() }}</h2>
+</template>
+
+<style scoped></style>
