b19e117154a5a1c98ae5c50bb6b1907b79060ab3
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-16 16:03:17 +0600

    Vue Js Event Handling

---
+++ form-input-bindings/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import InputFormBindings from './components/InputFormBindings.vue';
+
+
+</script>
+
+<template>
+  <InputFormBindings />
+</template>
+
+<style scoped></style>
---
+++ form-input-bindings/src/components/InputFormBindings.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { computed, ref } from 'vue';
+const name = ref("Devil");
+function greet(event) {
+    alert(`Hello ${name.value}!`);
+
+    if (event) {
+        alert(event.target.tagName);
+    }
+}
+
+const sayHello = () => {
+    alert('Say Hello');
+}
+
+const sayBye = () => {
+    alert('Say Bye');
+}
+
+const firstName = ref("");
+const lastName = ref('')
+
+const fullName = () => {
+    return `${firstName.value}  ${lastName.value}`;
+}
+
+</script>
+<template>
+    <h1>Event Handling</h1>
+
+    <button @click="greet">Greet</button>
+
+    <button @click="sayHello">Say Hello</button>
+    <button @click="sayBye">Say Bye</button>
+
+    <form action="get" @submit.prevent="fullName">
+        <input type="text" name="firsName" v-model="firstName" placeholder="Enter Your First Name">
+        <input type="text" name="lastName" v-model="lastName" placeholder="Enter Your Last Name">
+        <button type="submit">Submit</button>
+    </form>
+
+    <div>{{ fullName() }}</div>
+
+</template>
+<style scoped></style>
