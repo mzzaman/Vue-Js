c6e64be38832aa62fdb0ff9a4f012a560aff7cf0
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-17 21:54:11 +0600

    Task Management Project Added

---
+++ task-management/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import TaskManagement from './components/TaskManagement.vue';
+</script>
+
+<template>
+  <TaskManagement />
+</template>
+
+<style>
+body {
+  font-family: 'Arial', sans-serif;
+  padding: 20px;
+  font-size: 16px;
+  background-color: #f4f4f9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
