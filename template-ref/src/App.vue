01206ec8d6ec0e4263f9cf8be59f880100435d7b
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-17 21:19:15 +0600

    Vue Js Template Ref

---
+++ template-ref/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TemplateRef from "./components/TemplateRef.vue";
+
+
+</script>
+
+<template>
+    <TemplateRef />
+</template>
+
+<style scoped></style>
---
+++ template-ref/src/components/TemplateRef.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+
+const newTaskInput = ref(null);
+
+onMounted(() => {
+    newTaskInput.value.focus();
+})
+
+</script>
+<template>
+    <form>
+        <!-- Adding tasks form -->
+        <input placeholder="Add new task" ref="newTaskInput">
+        <select>
+            <option disabled value="">Select priority</option>
+            <option>High</option>
+            <option>Medium</option>
+            <option>Low</option>
+        </select>
+        <button type="submit">Add Task</button>
+    </form>
+
+</template>
+
+<style scoped></style>
