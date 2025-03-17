4db235b268063b4017e57d5c19d79b426a68162c
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-17 20:59:49 +0600

    Vue Js Lifecycle Hooks

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import OnMounted from './components/OnMounted.vue';
+
+
+</script>
+
+<template>
+    <OnMounted />
+</template>
+
+<style scoped></style>
---
+++ lifecycle-hooks/src/components/OnMounted.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onBeforeMount, onMounted } from 'vue';
+
+
+onMounted(() => {
+    console.log("mounted")
+});
+
+onBeforeMount(() => {
+    console.log("onBeforeMount")
+})
+
+</script>
+
+<template>
+    <h1>Vue Js Lifecycle Hooks</h1>
+
+</template>
+
+<style scoped></style>
