1f1413d5cabeeaa6ad7fc642851970492a0edc34
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-18 22:42:47 +0600

    Event Management Project Added

---
+++ event-managment/src/components/BookingItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import SectionCard from "@/components/SectionCard.vue";
+  import Button from "@/components/Button.vue";
+</script>
+
+
+<template>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div>Booking to Vue Conference</div>
+      <div>
+        <Button variant="danger">Cancel</Button>
+      </div>
+    </div>
+
+  </SectionCard>
+</template>
