ce55a58aedb33050bfa2f44fb54d5949c809b763
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-19 13:59:08 +0600

    Event Management Fake Data Fetching

---
+++ event-management/src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import EventCard from "@/components/EventCard.vue";
+import BookingItem from "@/components/BookingItem.vue";
+import LoadingEventCard from "./components/LoadingEventCard.vue";
+import LoadingBookingItem from "./components/LoadingBookingItem.vue";
+
+const events = ref([]);
+const eventLoading = ref(false);
+const bookings = ref([]);
+const bookingLoading = ref(false);
+
+const fetchEvents = async () => {
+  eventLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    events.value = await response.json();
+  } finally {
+    eventLoading.value = false;
+  }
+
+}
+
+const bookingFetch = async () => {
+  bookingLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/bookings');
+    bookings.value = await response.json();
+  } finally {
+    bookingLoading.value = false;
+  }
+}
+
+onMounted(() => {
+  fetchEvents(),
+    bookingFetch()
+});
+
+
+const handleRegistration = async (event) => {
+  const newBooking = {
+    id: Date.now().toString(),
+    userId: 1,
+    eventId: event.id,
+    eventTitle: event.title
+  }
+  await fetch('http://localhost:3001/bookings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...newBooking,
+      status: 'confirmed'
+    })
+  })
+}
+
+</script>
+
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class='text-4xl font-medium'>Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Event</h2>
+
+    <section class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <template v-if="!eventLoading">
+        <EventCard v-for="event in events" :key="event.id" :title="event.title" :when="event.date"
+          :description="event.description" @register="handleRegistration(event)" />
+      </template>
+
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
+
+    </section>
+
+
+
+    <h2 class="text-2xl font-medium">Your Booking</h2>
+    <section class="grid sm:grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <template v-if="!bookingLoading">
+        <BookingItem v-for="booking in bookings" :key="booking.id" :title="booking.eventTitle" />
+      </template>
+
+      <template v-else>
+        <LoadingBookingItem v-for="i in 4" :key="i" />
+      </template>
+    </section>
+
+
+  </main>
+
+</template>
---
+++ event-management/src/components/BookingItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SectionCard from "@/components/SectionCard.vue";
+import Button from "@/components/Button.vue";
+defineProps({
+  title: String,
+})
+</script>
+
+
+<template>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div>{{ title }}</div>
+      <div>
+        <Button variant="danger">Cancel</Button>
+      </div>
+    </div>
+
+  </SectionCard>
+</template>
